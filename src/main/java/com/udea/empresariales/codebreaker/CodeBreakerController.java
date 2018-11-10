a42226a01e35d6c4e827cfabebaa6c013314fafa
alejo <hildyale@gmail.com>
2018-11-10 11:39:48 -0500

    Cross

--- src/main/java/com/udea/empresariales/codebreaker/CodeBreakerController.java
+++ src/main/java/com/udea/empresariales/codebreaker/CodeBreakerController.java
@@ -1,6 +1,7 @@
 package com.udea.empresariales.codebreaker;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/guess")
 public interface CodeBreakerController {
 
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/{number}", produces = { "application/json" }, method = RequestMethod.GET)
 	ResponseEntity<CodeBreakerDTO> guess(@PathVariable("number") String number);
 
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "", produces = { "application/json" }, consumes = {
 			"application/json" }, method = RequestMethod.POST)
 	ResponseEntity<CodeBreakerDTO> generateSecret(@RequestBody CodeBreakerDTO codeBreaker);
