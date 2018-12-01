7061e2e72e4ac33e6d9adaf76ce6bc6c66b1119c
pabloromero0428 <pablo.romero2840@gmail.com>
2018-12-01 11:06:21 -0500

    correction error names

--- src/test/java/com/udea/empresariales/codebreaker/CodeBreakerTest.java
+++ src/test/java/com/udea/empresariales/codebreaker/CodeBreakerTest.java
@@ -9,88 +9,88 @@ public class CodeBreakerTest {
 	CodeBreaker codeBreaker = new CodeBreaker("1234");
 	
 	@Test
-	public void all_match() {
+	public void allMatch() {
 		String answer = codeBreaker.guess("1234");
 		assertEquals("XXXX", answer);
 	}
 	
 	@Test
-	public void first_match() {
+	public void firstMatch() {
 		String answer = codeBreaker.guess("1987");
 		assertEquals("X", answer);
 	}
 	
 	@Test
-	public void two_match() {
+	public void twoMatch() {
 		String answer = codeBreaker.guess("1984");
 		assertEquals("XX", answer);
 	}	
 	
 	@Test
-	public void contains_all_in_diferent_position() {
+	public void containsAllInDiferentPosition() {
 		String answer = codeBreaker.guess("4321");
 		assertEquals("____", answer);
 	}	
 	
 	@Test
-	public void contains_two_in_diferent_position() {
+	public void containsTwoInDiferentPosition() {
 		String answer = codeBreaker.guess("4891");
 		assertEquals("__", answer);
 	}
 	
 
 	@Test
-	public void contains_two_in_diferent_and_one_same_position() {
+	public void containsTwoInDiferentAndOneSamePosition() {
 		String answer = codeBreaker.guess("4831");
 		assertEquals("X__", answer);
 	}
 	
 	@Test
-	public void contains_nothing_match() {
+	public void containsNothingMatch() {
 		String answer = codeBreaker.guess("9867");
 		assertEquals("", answer);
 	}
 	
 	@Test
-	public void contains_two_in_diferent_and_two_same_position() {
+	public void containsTwoInDiferentAndTwoSamePosition() {
 		String answer = codeBreaker.guess("2134");
 		assertEquals("XX__", answer);
 	}
 	
 	@Test
-	public void contains_one_in_diferent_and_two_same_position() {
+	public void containsOneInDiferentAndTwoSamePosition() {
 		String answer = codeBreaker.guess("4230");
 		assertEquals("XX_", answer);
 	}
 	
 	@Test
-	public void more_digits_than_expected() {
+	public void moreDigitsThanExpected() {
 		String answer = codeBreaker.guess("42300");
 		assertEquals("", answer);
 	}
 	
 	@Test
-	public void less_digits_than_expected() {
+	public void lessDigitsThanExpected() {
 		String answer = codeBreaker.guess("425");
 		assertEquals("", answer);
 	}
 	
 	@Test
-	public void null_parameter() {
+	public void nullParameter() {
 		String answer = codeBreaker.guess(null);
 		assertEquals("", answer);
 	}
 	
 
 	@Test
-	public void all_match_v2() {
+	public void allMatchV2() {
 		codeBreaker = new CodeBreaker("4589");
 		String answer = codeBreaker.guess("4589");
 		assertEquals("XXXX", answer);
 	}
 	
 	@Test
-	public void first_match_v2() {
+	public void firstMatchV2() {
 		codeBreaker = new CodeBreaker("4589");
 		String answer = codeBreaker.guess("1523");
 		assertEquals("X", answer);
